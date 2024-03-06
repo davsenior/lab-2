c75d70c687dd8c30c5e9fe58a9eab88bef105a0c
davsenior <david.senior@mygeorgian.ca>
2024-03-06 15:47:24 -0500

    Started constructor 4 HW

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Constructor for Hot Wheels car
+function HotWheelsCar(model, color, series, year, size, material, packagingType, recommendedAge, features, price, manufacturerWebsite) {
+    this.model = model;
+    this.color = color;
+    this.series = series;
+    this.year = year;
+    this.size = size;
+    this.material = material;
+    this.packagingType = packagingType;
+    this.recommendedAge = recommendedAge;
+    this.features = features;
+    this.price = price;
+    this.manufacturerWebsite = manufacturerWebsite;
+}
